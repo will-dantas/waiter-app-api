d1c2590d4cdbc7cf97a97c9ad1b770b81c743d14
will-dantas <wmdantas2@gmail.com>
2023-01-25 18:15:31 -0300

    Create models API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import express from 'express';
+import express from "express";
+import moogoose from "mongoose";
 
 const app = express();
+
+moogoose
+  .connect("mongodb://localhost:27017")
+  .then(() => console.log("conectado ao mongo"))
+  .catch(() => console.log("Erro ao conectar ao mongo"));
 
 const port = 3001;
 
