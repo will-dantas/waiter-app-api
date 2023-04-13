2449234e7d4b1d24f158625ac6960882083f0008
will-dantas <wmdantas2@gmail.com>
2023-04-12 22:01:33 -0300

    Fix quotes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import express from "express";
-import moogoose from "mongoose";
-import path from "node:path";
-import cors from "cors";
-import { CreateCategoryRouter } from "./app/use-cases/categories/create-category/router";
-import { ListCategoriesRouter } from "./app/use-cases/categories/list-categories/router";
-import { ListProductsByCategoryRouter } from "./app/use-cases/categories/list-products-by-category/router";
-import { ChangeOrderRouter } from "./app/use-cases/orders/cancel-order/router";
-import { ChangeOrderStatusRouter } from "./app/use-cases/orders/change-order-status/router";
-import { CreateOrderRouter } from "./app/use-cases/orders/create-order/router";
-import { ListOrdersRouter } from "./app/use-cases/orders/list-orders/router";
-import { CreateProductRouter } from "./app/use-cases/products/create-product/router";
-import { ListProductsRouter } from "./app/use-cases/products/list-products/router";
+import express from 'express';
+import moogoose from 'mongoose';
+import path from 'node:path';
+import cors from 'cors';
+import { CreateCategoryRouter } from './app/use-cases/categories/create-category/router';
+import { ListCategoriesRouter } from './app/use-cases/categories/list-categories/router';
+import { ListProductsByCategoryRouter } from './app/use-cases/categories/list-products-by-category/router';
+import { ChangeOrderRouter } from './app/use-cases/orders/cancel-order/router';
+import { ChangeOrderStatusRouter } from './app/use-cases/orders/change-order-status/router';
+import { CreateOrderRouter } from './app/use-cases/orders/create-order/router';
+import { ListOrdersRouter } from './app/use-cases/orders/list-orders/router';
+import { CreateProductRouter } from './app/use-cases/products/create-product/router';
+import { ListProductsRouter } from './app/use-cases/products/list-products/router';
 
 moogoose
-  .connect("mongodb://localhost:27017")
+  .connect('mongodb://localhost:27017')
   .then(() => {
     const corsOptions = {
-      origin: "*",
+      origin: '*',
     };
 
     const app = express();
@@ -24,13 +24,13 @@ moogoose
 
     const port = 3001;
     app.use(
-      "/uploads",
-      express.static(path.resolve(__dirname, "..", "uploads"))
+      '/uploads',
+      express.static(path.resolve(__dirname, '..', 'uploads'))
     );
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.listen(port, () => {
-      console.log(`🚀 Server is running on http://localhost:${port}`);
+      console.log(`🚀 Server is running on http://172.18.100.109:${port}`);
     });
 
     // categories
@@ -48,4 +48,6 @@ moogoose
     new ChangeOrderStatusRouter(app).execute();
     new ChangeOrderRouter(app).execute();
   })
-  .catch(() => console.log("Error connecting to mongo"));
+  .catch(() => console.log('Error connecting to mongo'));
+
+//[{"name": "mussarela", "icon": "🧀"}, {"name": "catupiry", "icon": "🧀"},{"name": "frango", "icon": "🐔"}]
